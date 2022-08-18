798f0121b08572f8889c08a29dc330192dab808e
ljsomm <miscrits.lucas@gmail.com>
2022-08-17 21:10:23 -0300

    js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,4 +1,5 @@
 document.getElementById("btn-redirecionamento").addEventListener("click", () => {
+    alert(22);
     const nome = document.getElementById("nm-usuario").value;
     localStorage.setItem('nome', nome);
     window.location = "chat.html";
