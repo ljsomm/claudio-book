b95b8f63d0dae3387bfe82f388f6f774349f7520
ljsomm <miscrits.lucas@gmail.com>
2022-08-17 21:10:53 -0300

    bugfix

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,5 +1,4 @@
 document.getElementById("btn-redirecionamento").addEventListener("click", () => {
-    alert(22);
     const nome = document.getElementById("nm-usuario").value;
     localStorage.setItem('nome', nome);
     window.location = "chat.html";
